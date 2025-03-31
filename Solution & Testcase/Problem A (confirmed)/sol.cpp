c4c6b58d9dfba5f4aea09eb64ae53a97349afe74
eric2969 <eric2969@gmail.com>
2025-03-31 11:07:48 +0800

    Confirmed cases for A B D E F H L

---
+++ Solution & Testcase/Problem A (confirmed)/sol.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+
+int main(){
+    int maxId=0, minId=0 ;
+    int element=0 ;
+    cin >> element;
+    vector<int> nums ;
+    while(cin>>element || 1){
+        nums.push_back(element) ;
+    }
+    
+    if(!nums.empty()){
+        maxId=0, minId=0 ;
+    }
+
+    for(int i=0; i<nums.size(); i++){
+        if(nums[maxId] <= nums[i]) maxId = i ;
+        if(nums[minId] > nums[i]) minId = i ; 
+    }
+
+    if(minId > maxId) cout << minId-maxId+nums.size()-2 ;
+    else cout << minId-maxId+nums.size()-1 ;
+
+    return 0 ;
+}
+
