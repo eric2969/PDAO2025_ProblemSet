5b1f3f90547b04e73d6f82fe21f32af6afc07787
eric2969 <eric2969@gmail.com>
2025-04-01 14:00:21 +0800

    Fixed parameter name and fixed 0-based

--- Solution & Testcase/Problem D/sol.cpp
+++ Solution & Testcase/Problem D/sol.cpp
@@ -6,7 +6,7 @@ public:
     int findTheWinner(int n, int k) {
         int s = 0; //一開始的編號
         for(int i = 2; i <= n; i++) s = (s+k) % i; //第 i 輪中，他的位置是第 s 
-        return s+1; //如果題目的編號一開始是 1，那我們就加一
+        return s; //如果題目的編號一開始是 1，那我們就加一
     }
 };
 
@@ -18,7 +18,7 @@ int main() {
     cin >> n >> k;
     for(int i = 0;i < n;i++)
         cin >> people[i] >> mod[i];
-    int result = sol.findTheWinner(n, k) - 1;
-    cout << result+1 << ' ' << sol.findTheWinner(people[result], mod[result]) << endl;
+    int result = sol.findTheWinner(n, k);
+    cout << result << ' ' << sol.findTheWinner(people[result], mod[result]) << endl;
     return 0;
 }
