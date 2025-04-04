27dc08b6e5112628d6c9cf7725cad4330a99e47e
eric2969 <eric2969@gmail.com>
2025-04-05 02:03:52 +0800

    Adjust pL constraint (SegTree will failed)

--- Solution & Testcase/Problem L (confirmed)/sol.cpp
+++ Solution & Testcase/Problem L (confirmed)/sol.cpp
@@ -1,134 +1,69 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define X first
+#define Y second
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0)
+template<class T> bool chmin(T &a, T b) { return b < a && (a = b, true); }
+template<class T> bool chmax(T &a, T b) { return a < b && (a = b, true); }
+typedef long long ll;
+const int MAXn = 100000;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
 
-const int MAXN = 20000 + 5;
-
-struct SegmentTree {
-    long long sum[4 * MAXN], max_height[4 * MAXN], lazy[4 * MAXN];
-
-    void build(vector<int> &arr, int node, int start, int end) {
-        if (start == end) {
-            sum[node] = max_height[node] = arr[start];
-            return;
-        }
-        int mid = (start + end) / 2;
-        build(arr, node * 2, start, mid);
-        build(arr, node * 2 + 1, mid + 1, end);
-        sum[node] = sum[node * 2] + sum[node * 2 + 1];
-        max_height[node] = max(max_height[node * 2], max_height[node * 2 + 1]);
-    }
-    void push_down(int node, int start, int end) {
-        if (lazy[node] != 0) {
-            int mid = (start + end) / 2;
-            sum[node * 2] += lazy[node] * (mid - start + 1);
-            sum[node * 2 + 1] += lazy[node] * (end - mid);
-            max_height[node * 2] += lazy[node];
-            max_height[node * 2 + 1] += lazy[node];
-            lazy[node * 2] += lazy[node];
-            lazy[node * 2 + 1] += lazy[node];
-            lazy[node] = 0;
-        }
-    }
-
-    void update(int node, int start, int end, int l, int r, int val) {
-        if (l > end || r < start) return;
-
-        if (l <= start && end <= r) {
-            sum[node] += (end - start + 1) * val;
-            max_height[node] += val;
-            lazy[node] += val;
-            return;
-        }
-
-        push_down(node, start, end);
-        int mid = (start + end) / 2;
-        update(node * 2, start, mid, l, r, val);
-        update(node * 2 + 1, mid + 1, end, l, r, val);
-        sum[node] = sum[node * 2] + sum[node * 2 + 1];
-        max_height[node] = max(max_height[node * 2], max_height[node * 2 + 1]);
-    }
-
-    void reduce(int node, int start, int end, int l, int r, int val) {
-        if (l > end || r < start) return;
-
-        if (l <= start && end <= r) {
-            max_height[node] = max_height[node] - val;
-            sum[node] = sum[node] - (end - start + 1) * val;
-            lazy[node] -= val;
-            return;
-        }
-
-        push_down(node, start, end);
-        int mid = (start + end) / 2;
-        reduce(node * 2, start, mid, l, r, val);
-        reduce(node * 2 + 1, mid + 1, end, l, r, val);
-        sum[node] = sum[node * 2] + sum[node * 2 + 1];
-        max_height[node] = max(max_height[node * 2], max_height[node * 2 + 1]);
-    }
-
-    long long query_sum(int node, int start, int end, int l, int r) {
-        if (l > end || r < start) return 0;
-        if (l <= start && end <= r) return sum[node];
-        push_down(node, start, end);
-        int mid = (start + end) / 2;
-        return query_sum(node * 2, start, mid, l, r) + query_sum(node * 2 + 1, mid + 1, end, l, r);
-    }
-
-    long long query_max(int node, int start, int end, int l, int r) {
-        if (l > end || r < start) return 0;
-        if (l <= start && end <= r) return max_height[node];
-        push_down(node, start, end);
-        int mid = (start + end) / 2;
-        return max(query_max(node * 2, start, mid, l, r), query_max(node * 2 + 1, mid + 1, end, l, r));
-    }
-};
-
-void query_value(SegmentTree segTree, int N, int LEN, int P1, int P2){
-    long long best_price = 0; int best_index = 0;
-    for (int L = 1; L <= N - (LEN - 1); L++) {
-        long long maxH = segTree.query_max(1, 1, N, L, L + LEN - 1);
-        long long totalSum = segTree.query_sum(1, 1, N, L, L + LEN - 1);
-        long long price = (maxH * P1) + (totalSum * P2);
-        if(price > best_price) {
-            best_price = price;
-            best_index = L;
-        }
-    }
-    if(best_price == 0)
-        cout << "Oiiaioiiiai\n";
-    else
-        cout << best_index << " " << best_index+LEN-1 << " " << best_price << '\n';
-}
-
+ll h[MAXn + 5];
+ll diff[MAXn + 5];
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    IOS;
+    int n, q;
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++) {
+        cin >> h[i];
+        diff[i] = h[i] - h[i - 1];
+    }
 
-    int N, Q, P1, P2, LEN;
-    cin >> N >> Q;
+    while (q--){
+        int cmd; cin >> cmd;
+        if (cmd <= 2){
+            ll l, r, v; cin >> l >> r >> v;
+            v *= (cmd == 2 ? -1 : 1);
+            diff[l + 1] += v;
+            diff[r + 2] -= v;
+        }
+        else {
+            ll len, p1, p2;
+            cin >> len >> p1 >> p2;
 
-    vector<int> arr(N + 1);
-    for (int i = 1; i <= N; i++)
-        cin >> arr[i];
+            for (int i = 1; i <= n; i++)
+                h[i] = h[i - 1] + diff[i];
 
-    SegmentTree segTree;
-    segTree.build(arr, 1, 1, N);
+            deque<pair<ll, int>> dq;
+            ll s = 0;
+            auto Push = [&](int i){
+                while (!dq.empty() && dq.back().X <= h[i]) dq.pop_back();
+                dq.push_back(make_pair(h[i], i));
+            };
 
-    while (Q--) {
-        int type, L, R, val;
-        cin >> type;
-        if (type == 1) {
-            cin >> L >> R >> val;
-            segTree.update(1, 1, N, L, R, val);
-        } else if (type == 2) {
-            cin >> L >> R >> val;
-            segTree.reduce(1, 1, N, L, R, val);
-        } else if (type == 3) {
-            cin >> LEN >> P1 >> P2;
-            query_value(segTree, N, LEN, P1, P2);
+            ll cur_s = 0;
+            for (int i = 1; i <= len; i++) {
+                Push(i);
+                cur_s += h[i];
+            }
+            ll ans = -INF;
+            int l = 0;
+            for (int i = 1; i + len - 1 <= n; i++){
+                if (chmax(ans, 1ll * dq.front().X * p1 + 1ll * cur_s * p2))
+                    l = i;
+
+                // transit
+                if (i + len <= n) {
+                    cur_s -= h[i];
+                    cur_s += h[i + len];
+                    if (dq.front().Y == i) dq.pop_front();
+                    Push(i + len);
+                }
+            }
+
+            if (ans > 0) cout << l-1 << ' ' << l + len - 2 << ' ' << ans << '\n';
+            else cout << "Oiiaioiiiai\n";
         }
     }
     return 0;
