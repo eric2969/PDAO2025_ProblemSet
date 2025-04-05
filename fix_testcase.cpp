be003da5a804c79af535d8bfaeea413f5b888f55
eric2969 <eric2969@gmail.com>
2025-04-05 17:40:28 +0800

    Problem K fixed testcase

--- fix_testcase.cpp
+++ fix_testcase.cpp
@@ -2,15 +2,20 @@
 
 using namespace std;
 int main(){
-    int a, b;
+    string D[1005];
+    int p[1005], n;
     ifstream in;
     ofstream out;
-    for(int i=1;i<=100;i++){
-    	in.open(to_string(i)+".out");
-    	in >> a;
+    for(int i=1;i<=101;i++){
+    	in.open(to_string(i)+".in");
+    	in >> n;
+        for(int j=0;j<n;j++)
+            in >> D[j] >> p[j];
     	in.close();
-    	out.open(to_string(i)+".out");
-    	out << a-1 << endl;
+    	out.open(to_string(i)+".in");
+        out << n << endl;
+        for(int j=0;j<n;j++)
+            out << D[j] << " " << p[j]-1 << endl;
     	out.close();
     }
     cout << "Done";
